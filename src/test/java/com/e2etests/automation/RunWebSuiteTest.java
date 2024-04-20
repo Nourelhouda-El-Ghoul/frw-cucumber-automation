b83d4b5bcc17c1601d641c812c4915954380cea5
Nourlhouda El Ghoul <nourelhoudaelghoulqa@gmail.com>
2024-04-20 18:25:56 +0200

    test jenkins login

--- src/test/java/com/e2etests/automation/RunWebSuiteTest.java
+++ src/test/java/com/e2etests/automation/RunWebSuiteTest.java
@@ -11,7 +11,7 @@ import static io.cucumber.junit.CucumberOptions.SnippetType.CAMELCASE;
 @CucumberOptions(
 		features = {"src/spec/features"},
 		plugin = {"pretty","html:target/cucumber-report.html"},
-		tags = "@register",
+		tags = "@connexion",
 		//glue= {"com.e2etests.automation"},
 		monochrome = true,snippets = CAMELCASE
 		)
